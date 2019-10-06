b4b2c09bad280e38f65131ac0c2bc3ec4cedd8de
Chiho <yyflying11d@163.com>
2019-10-06 19:19:21 +0800

    commit

--- Vue/webpack-study/src/main.js
+++ Vue/webpack-study/src/main.js
@@ -1,7 +1,24 @@
 // main.js是项目的js入口文件
-// cnpm install jquery -s  在当前目录本地安装jquery
+// cnpm install jquery -S  在当前目录本地安装jquery
 import $ from 'jquery'  //es6语法
 // const $ = require('jquery')
+
+// 使用import语法，导入CSS样式表
+import './css/index.css'
+// import './css/index.less'
+// import './css/index.scss'
+
+// cnpm install style-loader css-loader less less-loader node-sass sass fibers sass-loader -D
+// 注意：webpack默认只能打包处理js类型的文件，无法处理其他的非js类型的文件
+// 如果要处理非js类型的文件，我们需要手动安装一些合适的第三方loader加载器
+  // 1.如果想要打包处理css文件，需要安装 cnpm i style-loader css-loader -D 
+  // 2.打开webpack.config.js这个配置文件，新增一个配置节点module，它是一个对象，有一个rules属性是个数组，存放了所有第三方文件的匹配和处理规则
+
+// webpack处理第三方类型文件的过程：
+  // 1.发现要处理的文件不是js文件，就去webpack.config.js配置文件中查找有没有对应的第三方loader规则
+  // 2.如果能找到，就调用相应的loader处理这种类型
+  // 3.调用loader的时候，是从后往前调用的
+  // 4.当最后一个loader调用完毕，会把处理的结果直接交给webpack进行打包合并，最终输出到bundle.js中去
 
 $(function () {
   $('li:odd').css('backgroundColor', 'lightblue')
--- Vue/webpack-study/webpack.config.js
+++ Vue/webpack-study/webpack.config.js
@@ -11,10 +11,29 @@
 // 配置文件就是配置这个命令：webpack ./src/main.js --output ./dist/bundle.js --mode development
 // 当写好配置文件后，只需命令行输入webpack即可实现打包
 // 使用 webpack-dev-server 这个工具可以实现自动打包编译的功能
-// cnpm install webpack-dev-server --save-dev  
-// 其中-save是在package.json中添加依赖"devDependencies"，-dev是在本地node_modules中安装包
+// cnpm install webpack-dev-server -d
+// 由于是在本地安装的webpack-dev-server，无法把它当作脚本命令，直接在终端中运行，只有安装到全局-g的工具，才能直接在终端输入运行
+// 可以在package.json中的"script"属性中添加一行 "dev": "webpack-dev-server --open --contentBase src --hot"，即可npm run dev 运行
+// --open 自动打开
+// --contentBase src 以src这个文件夹为根路径
+// --port 3000 设置端口号为3000
+// --hot 内容更新后以补丁的形式加上，不会每次都生成一份新的bundle.js，并且可以实现样式更改后，无刷新的更新页面
+
+// 同时，引入应该这样写<script src="/bundle.js"></script>，因为这个插件自动生成的bundle.js文件直接托管在了内存中，而不是磁盘上
+// <script src="../dist/bundle.js"></script>这样写是根本找不到修改后的bundle.js的
+// 修改完成后还是需要运行webpack更新一下磁盘上的bundle.js
+
+
 
 const path = require('path')
+
+// 导入在内存中生成html页面的插件
+// 只要是插件，就一定要放到plugins节点中去
+// 这个插件有两个作用：
+//   1.自动在内存中根据指定页面生成一个内存中的页面
+//   2.自动把打包好的bundle.js追加到页面中去
+const htmlWebpackPlugin = require('html-webpack-plugin')
+
 // 通过node的模块操作，向外暴露了一个配置对象
 module.exports = {
   mode: 'development',
@@ -23,5 +42,27 @@ module.exports = {
     // path: path.join(__dirname, './dist'), //打包好的文件要输出到哪个目录
     path: path.resolve(__dirname, 'dist'), //这里dist和./dist是一样的
     filename: 'bundle.js' //输出的文件名称
+  },
+  plugins: [
+    new htmlWebpackPlugin({ //创建一个 在内存中生成html页面的插件
+      template: path.join(__dirname, './src/index.html'),  //指定模板页面。将来会根据指定的页面路径，去生成内存中的页面
+      filename: 'index.html', //制定生成的页面的名称
+    })
+  ],
+  module: {  //这个节点用于配置所有第三方模块加载器
+    rules: [ // 所有第三方模块的模块的匹配和处理规则
+      {   //配置处理.css文件的第三方loader规则
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],//调用的顺序是从后往前调
+      },
+      {   //配置处理.less文件的第三方loader规则
+        test: /\.less$/,
+        use: ['style-loader', 'css-loader', 'less-loader'],
+      },
+      {   //配置处理.scss文件的第三方loader规则
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      }
+    ]
   }
 }
